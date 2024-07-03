22f97718b618eb44f4c01f5e690671414f39c7a7
binson thangjam <binsonsingh@gmail.com>
2024-07-03 23:25:58 +0530

    added analytics vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,6 +19,7 @@ export default function RootLayout({
       <body className={poppins.variable}>
         <NextAuthProvider>
         {children}
+        <Analytics/>
         <Toaster/>
         </NextAuthProvider>
         </body>
