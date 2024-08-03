6bd1f428cee80c855c07be6a80142dbadc14325d
binson thangjam <binsonsingh@gmail.com>
2024-08-04 00:38:04 +0530

    truly2 last commit

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -2,24 +2,26 @@ import TicketModel from "@/models/ticketModel";
 import dbConnect from "@/lib/dbConnect";
 import { NextResponse } from "next/server";
 import twilio from 'twilio';
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 const accountSid = process.env.TWILIO_ACCOUNTSID
 const authToken = process.env.TWILIOAUTHTOKEN
 const client = twilio(accountSid, authToken);
 export const POST = async (request) => {
-  const { name, place, amount,number } = await request.json();
+  const { name, place, amount, number } = await request.json();
   await dbConnect();
   try {
     await TicketModel.create({
       email: name, place, qty: amount
     });
     client.messages
-          .create({
-            body: `Hey ${name} your ticket is booked for ${place}`,
-            from: '+17862458469',
-            to: '+916009786052'
-          })
-          .then(message => console.log(message.sid));
+      .create({
+        body: `Hey ${name} your ticket is booked for ${place}`,
+        from: '+17862458469',
+        to: '+916009786052'
+      })
+      .then(message => console.log(message.sid));
     return new NextResponse("Ticket added", { status: 200 });
   } catch (err) {
     console.log(err);
@@ -30,11 +32,12 @@ export const POST = async (request) => {
 };
 
 export const GET = async (request) => {
+  const session = await getServerSession(authOptions)
   await dbConnect()
   try {
-    const tickets = await TicketModel.find({email:session.user.email})
+    const tickets = await TicketModel.find({ email: session.user.email })
     return NextResponse.json(tickets)
   } catch (error) {
-     return NextResponse.json(error)
+    return NextResponse.json(error)
   }
 }
